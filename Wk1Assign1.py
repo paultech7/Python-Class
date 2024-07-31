7cf2bd5e1136e57eebe03ec36d3dea936798e575
Paul <paulbrun700@gmail.com>
2024-07-31 18:32:57 -0400

    Add check for numerical input

--- Wk1Assign1.py
+++ Wk1Assign1.py
@@ -5,21 +5,21 @@ while principal == 0:
     if string1.isdigit():
         principal = int(string1)
     else:
-        print("Principal must be a number")
+        print("<< Principal must be a number. >>")
 rate = 0
 while rate == 0:
     string1 = input("Enter the interest rate as a percentage: ")
     if string1.isdigit():
         rate = int(string1)
     else:
-        print("Interest rate must be a number")
+        print("<< Interest rate must be a number. >>")
 years = 0
 while years == 0:
     string1 = input("Enter the number of years: ")
     if string1.isdigit():
         years = int(string1)
     else:
-        print("Years must be a number")
+        print("<< Years must be a number. >>")
 
 # calculate the simple interest
 simpleInt = (principal * rate * years) / 100
--- Wk1Assign2.py
+++ Wk1Assign2.py
@@ -3,9 +3,17 @@
 length = -1
 width = -1
 while length < 0:
-    length = int(input("Enter a positive number for the length: "))
+    string1 = input("Enter the length: ")
+    if string1.isdigit() and int(string1) > 0:
+        length = int(string1)
+    else:
+        print("<< Length must be a positive number. >>")
 while width < 0:
-    width = int(input("Enter a positive number for the width: "))
+    string1 = input("Enter the width: ")
+    if string1.isdigit() and int(string1) > 0:
+        width = int(string1)
+    else:
+        print("<< Width must be a positive number. >>")
 
 # calculate the area
 area = length * width
