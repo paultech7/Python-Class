1d309cf706c69d635ace9d5828e1d0f593dd86d2
Paul <paulbrun700@gmail.com>
2024-07-30 20:14:28 -0400

    Initial update

--- Wk1Assign1.py
+++ Wk1Assign1.py
@@ -1,3 +1,10 @@
 
-# single-line comment
-print("Here we go!")
+# Get the inputs for the calculation
+principal = int(input("Enter the principal amount: "))
+interest = int(input("Enter the interest as a percentage: "))
+years = int(input("Enter the number of years: "))
+
+simpleInt = (principal * interest * years) / 100
+
+print("")
+print("Simple interest is", simpleInt)
