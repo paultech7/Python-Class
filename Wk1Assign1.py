193b4f23ecee264510ce3e3e99694706433e926e
Paul <paulbrun700@gmail.com>
2024-07-31 18:21:10 -0400

    Add check for numerical input

--- Wk1Assign1.py
+++ Wk1Assign1.py
@@ -1,10 +1,27 @@
-
 # Get the inputs for the calculation
-principal = int(input("Enter the principal amount: "))
-rate = int(input("Enter the interest rate as a percentage: "))
-years = int(input("Enter the number of years: "))
+principal = 0
+while principal == 0:
+    string1 = input("Enter the principal amount: ")
+    if string1.isdigit():
+        principal = int(string1)
+    else:
+        print("Principal must be a number")
+rate = 0
+while rate == 0:
+    string1 = input("Enter the interest rate as a percentage: ")
+    if string1.isdigit():
+        rate = int(string1)
+    else:
+        print("Interest rate must be a number")
+years = 0
+while years == 0:
+    string1 = input("Enter the number of years: ")
+    if string1.isdigit():
+        years = int(string1)
+    else:
+        print("Years must be a number")
 
-# calculate the interest
+# calculate the simple interest
 simpleInt = (principal * rate * years) / 100
 
 print("")
