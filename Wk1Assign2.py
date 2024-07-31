391e5f01ffd5a5b8b37d5bfdb69f71ec1c6eab2d
Paul <paulbrun700@gmail.com>
2024-07-31 15:32:58 -0400

    Initial update

---
+++ Wk1Assign2.py
@@ -0,0 +1,9 @@
+
+# Get the inputs for the calculation
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+
+area = length * width
+
+print("")
+print("Area =", area)
