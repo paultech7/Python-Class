5ccf22d0a359126e92bd4c291afcc7898535ff00
Paul <paulbrun700@gmail.com>
2024-08-08 20:08:01 -0400

    Simplify to match assignment requirements

--- Assignment10.py
+++ Assignment10.py
@@ -1,19 +1,22 @@
 # Get the age
-age = 0
-while age == 0:
-    string1 = input("Enter your age: ")
-    if string1.isdigit() and int(string1) > 0:
-        age = int(string1)
+string1 = input("Enter your age: ")
+
+# Validate the input
+if string1.isdigit():
+    age = int(string1)
+    if age > 0:
+
+        # Calculate the category of the age and show the result
+        if age > 65:
+            category = "Senior citizen"
+        elif age < 18:
+            category = "Minor"
+        else:
+            category = "Adult"
+        print("Your age category is", category)
+
     else:
-        print("<< Please enter a positive integer for your age. >>")
-
-# Calculate the category of the age
-if age > 65:
-    category = "Senior citizen"
-elif age < 18:
-    category = "Minor"
+        print("Error: Please enter a positive integer for your age.")
 else:
-    category = "Adult"
+    print("Error: Please enter a positive integer for your age.")
 
-print("")
-print("Your age category is", category)
--- Assignment7.py
+++ Assignment7.py
@@ -1,22 +1,26 @@
 # Get the year to test
-year = 0
-while year == 0:
-    string1 = input("Enter the year: ")
-    if string1.isdigit() and int(string1) > 0:
-        year = int(string1)
+string1 = input("Enter the year: ")
+
+# Validate the input
+if string1.isdigit():
+    year = int(string1)
+    if year > 0:
+
+        # Determine if the year is a leap one
+        isLeapYear = False
+        if year - (4 * int(year / 4)) == 0:
+            if year - (100 * int(year / 100)) != 0:
+                isLeapYear = True
+            elif year - (400 * int(year / 400)) == 0:
+                isLeapYear = True
+
+        # Show the result
+        if isLeapYear:
+            print(year, "is a leap year.")
+        else:
+            print(year, "is not a leap year.")
+
     else:
-        print("<< Please enter a positive integer for the year. >>")
-
-# Test if the year is a leap one. Leap years are divisible by 4 but not by 100 unless it is also divisible by 400.
-isLeapYear = False
-if year - (4 * int(year / 4)) == 0:
-    if year - (100 * int(year / 100)) != 0:
-        isLeapYear = True
-    elif year - (400 * int(year / 400)) == 0:
-        isLeapYear = True
-
-print("")
-if isLeapYear:
-    print(year, "is a leap year.")
+        print("Error: the year must be a positive integer.")
 else:
-    print(year, "is not a leap year.")
+    print("Error: the year must be a positive integer.")
