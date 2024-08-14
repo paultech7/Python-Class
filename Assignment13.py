1665553ef0a0585b283e64d3ed83b08a11df5120
Paul <paulbrun700@gmail.com>
2024-08-14 19:11:52 -0400

    Initial update

---
+++ Assignment13.py
@@ -0,0 +1,19 @@
+# Get the starting integer
+string1 = input("Enter a string: ")
+
+# Test if the string is a palindrome by looping from the beginning and end of the string and comparing characters
+fromBegin = 0
+fromEnd = len(string1) - 1
+isPalindrome = True
+while fromBegin < fromEnd:
+    if string1[fromBegin] != string1[fromEnd]:
+        isPalindrome = False
+        break
+    fromBegin += 1
+    fromEnd -= 1
+
+# Show the result
+if isPalindrome:
+    print("String is a palindrome")
+else:
+    print("String is not a palindrome")
