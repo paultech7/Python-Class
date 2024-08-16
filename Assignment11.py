5ce3792f781f17a1d1b69c3e01a80ef646d043cd
Paul <paulbrun700@gmail.com>
2024-08-15 20:07:09 -0400

    Redo validation and use floor division

--- Assignment11.py
+++ Assignment11.py
@@ -1,24 +1,25 @@
 # Get the starting integer
-string1 = input("Enter a positive integer: ")
+inputString = input("Enter a positive integer: ")
 
 # Validate the input
-if string1.isdigit():
-    number = int(string1)
-    if number > 0:
-
-        # Calculate the Collatz sequence for the integer until it reaches 1.
-        seq = string1
-        while number != 1:
-            if number % 2 == 0:
-                number /= 2
-            else:
-                number = number * 3 + 1
-            seq += ", " + str(number)
-        else:
-            # Show the resulting sequence
-            print("Collatz sequence is (" + seq + ")")
-
+number = 0
+while number == 0:
+    if inputString.isdigit():
+        number = int(inputString)
+        if number <= 0:
+            inputString = input("Number must be a positive integer. Re-enter: ")
     else:
-        print("Error: Please enter a positive integer.")
-else:
-    print("Error: Please enter a positive integer.")
+        inputString = input("Invalid entry. Please enter a positive integer: ")
+
+# Calculate the Collatz sequence for the integer until it reaches 1.
+seq = inputString
+while number != 1:
+    if number % 2 == 0:
+        number //= 2
+    else:
+        number = number * 3 + 1
+    seq += ", " + str(number)
+
+# Show the resulting sequence
+print("")
+print("Collatz sequence is (" + seq + ")")
