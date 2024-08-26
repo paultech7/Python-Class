1d150cf5dcc204e714be22cd38ff20ea130d23e7
Paul <paulbrun700@gmail.com>
2024-08-26 18:55:59 -0400

    update is_prime function

--- Assignment16.py
+++ Assignment16.py
@@ -4,11 +4,16 @@ import math
 # Define a function that tests if the passed integer is a prime.
 # Returns: True if the number is prime; False if it is not.
 def is_prime(pos_int):
-    if pos_int <= 1:
+    if pos_int < 1:
+        return "Error: not a positive integer"
+
+    if pos_int == 1:
         return False
-    elif pos_int == 2:
+
+    if pos_int == 2:
         return True
-    elif pos_int % 2 == 0:
+
+    if pos_int % 2 == 0:
         return False        # all even numbers > 2 are not prime
     else:
         # Since we now know the number is not divisible by 2,
@@ -25,15 +30,15 @@ string_in = input("Enter a positive integer: ")
 while True:
     if string_in.isnumeric():
         numberIn = int(string_in)
-        if numberIn > 0:
-            break
-        else:
-            string_in = input("Error: number must be a positive integer. Re-enter: ")
+        break
     else:
-        string_in = input("Error: number must be a positive integer. Re-enter: ")
+        string_in = input("Error: entry must be a positive integer. Re-enter: ")
 
 # Test if the integer is a prime and show the result
-if is_prime(numberIn):
+result = is_prime(numberIn)
+if isinstance(result, str):
+    print(result)
+elif result is True:
     print("Number is prime.")
 else:
     print("Number is NOT prime.")
